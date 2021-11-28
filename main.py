e31ec349cef5d8cef62a4d69e30dbc0999eb0201
Jonathan Chagolla <90534449+chagolla23@users.noreply.github.com>
2021-11-28 01:02:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+user_height = float(height) 
+user_weight = float(weight)
+bmi = user_height / user_weight ** 2
+print("Your bmi is:", int(BMI))
+
+
+
+
+
+
+
+
+
